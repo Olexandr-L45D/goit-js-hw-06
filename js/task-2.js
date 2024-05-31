5f48ba2de7ae2408c190f0f2bc9cc88ae605d426
Olexandr-L45D <160649564+Olexandr-L45D@users.noreply.github.com>
2024-05-31 14:04:40 +0300

    task-2

---
+++ js/task-2.js
@@ -0,0 +1,33 @@
+class Storages {
+    #items;
+     constructor (items) {  this.#items = items;}
+     get items() { return  this.#items; }
+     set items(newItem) { this.#items = newItem}
+}
+class Storage extends Storages {
+    constructor (items, newItem, itemToRemove) {
+    super(items)
+    this.newItem = newItem;
+    this.itemToRemove = itemToRemove;
+    }
+     getItems() { 
+     return  this.items;
+    }
+     addItem(newItem) {
+        this.items.push(newItem);
+   }
+     removeItem(itemToRemove) {
+     this.items = this.items.filter((strin) => {return strin != itemToRemove});
+   }
+   }
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
