a042948de8619cd51fca4072e9108f89237cc589
Olexandr-L45D <160649564+Olexandr-L45D@users.noreply.github.com>
2024-06-02 12:14:01 +0300

    updat task-2-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,17 +1,15 @@
 class Storage {
     #items;
      constructor (items) {  this.#items = items;}
-     get items() { return  this.#items; }
-     set items(newItem) { this.#items = newItem}
-
+     
      getItems() { 
-     return  this.items;
+     return  this.#items;
     }
      addItem(newItem) {
-        this.items.push(newItem);
+        this.#items.push(newItem);
    }
      removeItem(itemToRemove) {
-     this.items = this.items.filter((strin) => {return strin != itemToRemove});
+     this.#items = this.#items.filter((strin) => {return strin != itemToRemove});
    }
    }
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
